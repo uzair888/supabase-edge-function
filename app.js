53a3b7f9561ed4b9a7a1afbfbbaa1754a783e7ac
uzair888 <uzairsarwar10@gmail.com>
2024-06-27 20:55:30 +0500

    update changes

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 // Import and use your edge function
 const { handler } = require("./getStudents");
+
+const allowedOrigins = ["http://localhost:3001/","http://localhost:3000/"];
+const corsOptions = {
+  credentials: true,
+  origin: allowedOrigins,
+  methods: "GET, POST, PUT, DELETE",
+  allowedHeaders: "Content-Type, Authorization, Cookie",
+};
+app.use(express.static("public"));
+app.use(cors(corsOptions));
 
 app.get("/api/students", async (req, res) => {
   const response = await handler(req, res);
